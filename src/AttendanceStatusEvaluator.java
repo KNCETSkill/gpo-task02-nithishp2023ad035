4afa51cb21184fc61d9755783d29a4175116b2ea
NITHISH P <nithishp2023ad035@kongunadu.ac.in>
2026-01-19 16:14:16 +0530

    Update AttendanceStatusEvaluator.java

--- src/AttendanceStatusEvaluator.java
+++ src/AttendanceStatusEvaluator.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
+
+public class StudentAttendanceEvaluator {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int Attendance = sc.nextInt();
+        if(Attendance>85){
+            printf("Excellent");
+        } else if(Attendance>=60 && Attendance<=85){
+            printf("Satisfactory");       
+        } else if(Attendance<60){
+            printf("Poor");
+                }
+       
+        sc.close();
+    }
+}
+
 
